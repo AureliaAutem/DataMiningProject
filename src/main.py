211df3ad2b4d0ab6414350e67b86edd8f4645ecb
LunarX <gibran.chevalley@etu.unige.ch>
2019-06-21 15:11:41 +0200

    Added writing methods and sub x coord methods

--- src/main.py
+++ src/main.py
@@ -6,20 +6,31 @@ import glob                     #To read filenames in folder
 #Our own functions
 from read_data import *
 from manipulation import *
+from write_data import *
 
 #We read all the data filenames
 root = "data/"
-labels = ["LASI", "RASI", "LPSI", "RPSI"]
+labels = ["RTOE", "RANK", "RHEE", "T10", "LTOE", "LANK", "LHEE"]
 
 #We separate the data
-(train, test) = split_train_test_files(root)
+# (train, test) = split_train_test_files(root)
+#
+# X_train = get_data_by_labels(labels, train, method="dense")
+# X_test = get_data_by_labels(labels, test, method="dense")
+#
+# print(X_train.shape)
+# print(X_test.shape)
 
-X_train = get_data_by_labels(labels, train, method="dense")
-X_test = get_data_by_labels(labels, test, method="dense")
+reader = btk.btkAcquisitionFileReader()
+reader.SetFilename("CP_GMFCS1_01916_20130128_18.c3d")
+reader.Update()
+acq = reader.GetOutput()
+metadata = acq.GetMetaData()
+point_labels = metadata.FindChild("POINT").value().FindChild("LABELS").value().GetInfo().ToString()
 
-print(X_train.shape)
-print(X_test.shape)
 
+acq = sub_x_coord(point_labels, 'CP_GMFCS1_01916_20130128_18.c3d', 'T10')
+write_acq(acq)
 
 #reading_data(filenames[0])
 # for i in range (len(filenames)) :
---
+++ src/write_data.py
@@ -0,0 +1,31 @@
+#Librairies
+from btk import btk             #To read the data in .c3d files
+import numpy as np
+from read_data import get_acquisition_from_data
+
+def sub_x_coord(labels, file, to_sub):
+    acq = get_acquisition_from_data(file)
+
+    sub_val = acq.GetPoint(to_sub).GetValues()[:, 0]
+
+    for label in labels :
+        label = label.strip()
+        # print('Working with [' + label + ']')
+
+        updatedX = acq.GetPoint(label).GetValues()[:, 0]
+        updatedX = updatedX - sub_val
+
+        for i in range(len(updatedX)):
+            acq.GetPoint(label).SetValue(i, 0, updatedX[i])
+
+        # print('Updated ' + label)
+        # print(acq.GetPoint(label).GetValues()[0:10, :])
+
+    return acq
+
+
+def write_acq(acq):
+     writer = btk.btkAcquisitionFileWriter()
+     writer.SetInput(acq)
+     writer.SetFilename('updated.c3d')
+     writer.Update()
