d995e8ba258c7e0d419bc8ae6cccb1f4061703c1
LunarX <gibran.chevalley@etu.unige.ch>
2019-06-22 13:44:06 +0200

    Added first part of the data visualisation

--- src/read_data.py
+++ src/read_data.py
@@ -226,3 +226,41 @@ def define_dense_labels(event_frames, event_labels, event_contexts, start_frame,
 #         vector[index:index+pad] = val
 #
 #     return vector
+
+def get_2D_disp_data(label, filename) :
+    """Can probably be deleted because it just returns the X values and the Y
+    labels separatly"""
+    # We create an acquisition to manipulate the file c3d
+    acq = get_acquisition_from_data(filename)
+    x = acq.GetPoint(label).GetValues()[:, 1:3]
+
+
+    # We extract the frames where there are events
+    n_events = acq.GetEventNumber() # Number of events
+    if (n_events == 0) :
+        print("There is no event !")
+        exit(-1)
+
+    event_frames = []
+    event_labels = []
+    event_contexts = []
+    for event in [acq.GetEvent(event) for event in range(n_events)] :
+        event_frames += [event.GetFrame()]
+        event_labels += [event.GetLabel()]
+        event_contexts += [event.GetContext()]
+
+    perm = np.argsort(event_frames)
+    event_frames = np.take(event_frames, perm)
+
+    event_labels = np.take(event_labels, perm)
+    event_labels = (np.array(event_labels) == 'Foot_Strike_GS')
+
+    event_contexts = np.take(event_contexts, perm)
+    event_contexts = (np.array(event_contexts) == 'Left')
+
+    first_frame = acq.GetFirstFrame()
+    start_frame = event_frames[0]-first_frame
+    end_frame = event_frames[-1]-first_frame
+    vector = define_dense_labels(event_frames, event_labels, event_contexts, start_frame, end_frame)
+
+    return x, vector
