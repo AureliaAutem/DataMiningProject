0e2b4d1fe3859d87b0920a8cf6454d3007998bdf
jade243 <aurelia.autem@etu.unige.ch>
2019-06-22 14:46:45 +0200

    Clean code and create a new file with functions linked to visualization

--- src/read_data.py
+++ src/read_data.py
@@ -217,6 +217,13 @@ def get_dense_data_from_file(labels, acq) :
     return X
 
 def sub_x_coord(labels, file, to_sub):
+    """Function which substract the value of 'to_sub' to all the points in
+    labels from the file 'file'
+    Inputs :    - labels : points
+                - file : relative path to the file
+                - to_sub : point we want to substract
+    Outputs :   - acq : acquisition with the modified points
+    """
     acq = get_acquisition_from_data(file)
 
     sub_val = acq.GetPoint(to_sub).GetValues()[:, 0]
@@ -333,42 +340,3 @@ def define_sparse_no_event_frames(event_frames, event_labels, event_contexts, st
         frames += [frame]
 
     return frames
-
-
-def get_2D_disp_data(label, filename) :
-    """Used to get (Y,Z) data and associated class for a single file for data
-    visualisation for kmeans"""
-    # We create an acquisition to manipulate the file c3d
-    acq = get_acquisition_from_data(filename)
-    x = acq.GetPoint(label).GetValues()[:, 1:3]
-
-
-    # We extract the frames where there are events
-    n_events = acq.GetEventNumber() # Number of events
-    if (n_events == 0) :
-        print("There is no event !")
-        exit(-1)
-
-    event_frames = []
-    event_labels = []
-    event_contexts = []
-    for event in [acq.GetEvent(event) for event in range(n_events)] :
-        event_frames += [event.GetFrame()]
-        event_labels += [event.GetLabel()]
-        event_contexts += [event.GetContext()]
-
-    perm = np.argsort(event_frames)
-    event_frames = np.take(event_frames, perm)
-
-    event_labels = np.take(event_labels, perm)
-    event_labels = (np.array(event_labels) == 'Foot_Strike_GS')
-
-    event_contexts = np.take(event_contexts, perm)
-    event_contexts = (np.array(event_contexts) == 'Left')
-
-    first_frame = acq.GetFirstFrame()
-    start_frame = event_frames[0]-first_frame
-    end_frame = event_frames[-1]-first_frame
-    vector = define_dense_labels(event_frames, event_labels, event_contexts, start_frame, end_frame)
-
-    return x, vector
